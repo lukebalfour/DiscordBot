79ffb0fa7b7ee21d895165909a17a837b43c38b9
Luke <37871010+lukebalfour@users.noreply.github.com>
2020-02-14 17:17:02 +0000

    o

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const token = "NjQ2NzU1NTg3Nzk5MTIxOTQw.XdWaPQ.ltxWGKiKBSc9vO5KRNh9O5uVPIA";
+const token = "NjQ2NzU1NTg3Nzk5MTIxOTQw.XdayQw.nZJosw8pryggtbL_7En6jDjZ2sA";
 
 const prefix = ".";
 const fs = require("fs");
@@ -9,82 +9,118 @@ bot.commands = new Discord.Collection();
 
 bot.login(token);
 
-fs.readdir("C:\Users\Luke PC\Desktop\Bot\cmds", (err, files) => {
-    if (err) console.error(err);
+//bot connecting
+bot.on('ready', () => {
+    bot.user.setActivity('twitch.tv/look_', { type: 'WATCHING' });
+    console.log("Bot Logged In!");
+});
 
-    let jsfiles = files.filter(f => f.split(".").pop === "js");
-    if (jsfiles.length <= 0) {
-        console.log("no commands to load");
-        return;
+
+bot.on('message', msg => {
+    //normal text responses
+    if (msg.content.toLowerCase() === 'pog') {
+        msg.channel.send('POG!');
+    }
+    else if (msg.content.toLowerCase() === 'nigger') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'nigga') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'niger') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'niga') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'n1gger') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'n1gga') {
+        msg.delete();
+        msg.channel.send('no no no, not that word')
+    }
+    else if (msg.content.toLowerCase() === 'fuck that guy') {
+        msg.channel.send('Yea fuck that guy am I right!');
+    }
+    else if (msg.content.toLowerCase() === 'bruh') {
+        msg.channel.send('BRUH BRUH BRUH');
+    }
+    else if (msg.content.toLowerCase() === 'joe') {
+        msg.channel.send('Joe who?');
+    }
+    else if (msg.content.toLowerCase() === '?') {
+        msg.channel.send('??');
+    }
+    else if (msg.content.toLowerCase() === 'carbonara') {
+        msg.channel.send('carbonara?');
+    }
+    else if (msg.content.toLowerCase() === 'fortnite') {
+        msg.channel.send('WE LOVE FORTNITE');
+    }
+    else if (msg.content.toLowerCase() === 'joe mama') {
+        msg.channel.send('FUCK');
     }
 
-    console.log(`Loading ${jsfiles.length} commands!`);
+    //auto person responses
+    else if (msg.author.id === '335387205386371075') {
+        msg.channel.send('BOT');
+    }
+    else if (msg.author.id === '254627144729100288') {
+        msg.channel.send('Who are you?');
+    }
+    else if (msg.author.id === '444612036543119360') {
+        msg.channel.send('STEVEN!');
+    }
+    else if (msg.author.id === '503720029456695306') {
+        msg.channel.send('Fuck off Dad.');
+    }
+    else if (msg.author.id === '279321020052013066') {
+        msg.channel.send('Fuck you Pussy');
+    }
 
-    jsfiles.forEach((f, i) => {
-        let props = require(`C:\Users\Luke PC\Desktop\Bot\cmds${f}`);
-        console.log(`${i + 1}: ${f} loaded!`);
-        bot.commands.set(props.help.name, props);
-    });
+        //mention messages
+    else if (msg.mentions.users === "161140743249854464") {
+        msg.channel.sens('Stop talking to the boomer');
+    }
+    else if (msg.mentions.users === "181011485936451584") {
+        msg.channel.sens('Do not anger Mr. Gesmashsmash');
+    }
+    else if (msg.mentions.users === "247360217673760769") {
+        msg.channel.sens('Temour cannot play right now as there is a cement truck in the way. Try again later.');
+    }
+    else if (msg.mentions.users === "323192589337755648") {
+        msg.channel.sens('Make sure your are being respectful when talking to a Co-Super Admin!');
+    }
+    else if (msg.mentions.users === "335386836128104449") {
+        msg.channel.sens('Make sure your are being respectful when talking to a Co-Super Admin!');
+    }
+
+    //commands
+    else if (msg.content.toLowerCase() === '.10man') {
+        msg.channel.send('@everyone 10 man happening message if you want in')
+    }
+    else if (msg.content.toLowerCase() === '.ira') {
+        msg.channel.send('We do not associate')
+    }
+    else if (msg.content.toLowerCase() === '.superadmin') {
+        msg.channel.send('Luke & Adam are currently Co-Super Admins')
+    }
+    else if (msg.content.toLowerCase() === '.twitch') {
+        msg.channel.send('https://www.twitch.tv/look_');
+        msg.channel.send('https://www.twitch.tv/iamcg');
+        msg.channel.send('https://www.twitch.tv/goyathlay_towy');
+        msg.channel.send('https://www.twitch.tv/jnicog');
+    }
+    else if (msg.content.toLowerCase() === '.commands') {
+        msg.channel.send('.10man, .ira, .superadmin, .twitch')
+    }
 });
 
-bot.on('ready', () =>{
-    console.log("Bot Logged In!");
-    console.log(bot.commands);
-} )
-
-bot.on("message", async message => {
-    if (message.author.bot) return;
-    if (message.channel.type === "dm") return;
-
-    let messageArray = message.message.content.split(/\s+/g);
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-
-    if (!command.startsWith(prefix)) return;
-
-    let cmd = bot.commands.get(command.slice(prefix.length));
-    if (cmd) cmd.run(bot, message, args);
-});
-
-
-//client.on('message', msg => {
-//    if (msg.content.toLowerCase() === 'pog')
-//        msg.channel.send('POG!');
-//    else if (msg.content === 'nigger') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content === 'nigga') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content === 'niger') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content === 'niga') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content === 'n1gger') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content === 'n1gga') {
-//        msg.delete();
-//        msg.channel.send('no no no, not that word')
-//    }
-//    else if (msg.content.toLowerCase() === 'fuck that guy')
-//        msg.channel.send('Yea fuck that guy am I right!');
-//    else if (msg.content.toLowerCase() === 'bruh')
-//        msg.channel.send('BRUH BRUH BRUH');
-//    else if (msg.content.toLowerCase() === '?10man')
-//        msg.channel.send('@everyone 10man happening message if you want in.');
-//    else if (msg.)
-//        msg.channel.send('BOT');
-//});
-
-
-function checkCommand(message, commandName) {
-    return message.content.toLowerCase().startsWith("?" + commandName);
-}
+// cameron thomp ID - 335387205386371075
+// my id - 335386836128104449
